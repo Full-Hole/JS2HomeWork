13f4ad2ca788b0a000d9e5a9eaa488e93748a653
Full Hole <timur91g@gmail.com>
2021-03-27 18:33:50 +0300

    Update code

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,25 @@
 'use strict'
 
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
-  
-  const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
-  }
-  
-  renderGoodsList(goods);
+const products = [
+  {id: 1, title: 'Notebook', price: 2000},
+  {id: 2, title: 'Mouse', price: 20},
+  {id: 3, title: 'Keyboard', price: 200},
+  {id: 4, title: 'Gamepad', price: 50},
+];
+//Функция для формирования верстки каждого товара
+const renderProduct = (product, img='img/1.jpg') => {
+  return `<div class="product-item">
+              <h3>${product.title}</h3>
+              <img src="${img}" class="product-img">
+              <p>${product.price}</p>
+              <button class="buy-btn">Купить</button>
+          </div>`
+};
+const renderPage = list => {
+  const productsList = list.map(item => renderProduct(item));
+  console.log(productsList);
+  document.querySelector('.products').innerHTML = productsList.join("");
+};
+
+renderPage(products);
   
